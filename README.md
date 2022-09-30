8caa5634790322712311c09284b02048bd697584
Michael Girnyak <michael@MacBook-Pro.local>
2022-09-30 11:31:30 +0300

    up

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
 # Assigment for cool people
 
-George Lucas once came to me (in my dream) the other day and asked. Michael what would it take to automate our X-wing model tracking? Could all your fancy AI tecnologies do that 
+George Lucas once came to us the other day and asked. Guys what would it take to automate our X-wing model tracking? 
+Could all your fancy AI technologies do that.
+
+We said, sure, we can do that. But we need to know what you want to do with it. 
+He said, well, as we use the X-wing models for our movies, we need to track them in order to add lasers after the fact. 
+We need to know where the lasers should be added.
+All this job were done by hand before, but we need to automate it. 
+Example of frames looks like this.
 
 ![xwing](frames/xwing.gif)
 
+Our goal is to be able to track the X-wing models in the frames and output animation with lasers added.
+Like this.
+
+![result](result/pewpew.gif)
+
 Now we are proposing you to go this path of force and solve this problem as good as you can.
-And may be some day George Lucas visit you (in your dream) to say "Great JOB I'm really proud that I know you personally"
+And may be some day George Lucas visit you to hire you for his next project.
+
+## Data
+In order to train your model we provide you with 100,000 random frames of X-wing models. 
+And its corresponding model matrices in `dataset` folder. 
+Also there is `camera_info.json` file with camera parameters necessary for projection of 3d point to 2d image.
+For additional information about the dataset you can read [Reference.ipynb](Reference.ipynb) notebook.
+
+
+## Task
+Your task is to train a model that will be able to track the X-wing models in the frames and output animation with lasers added.
+You can use any model you want, but we recommend you to look for a papers that use NN to predict keypoints.
+If your computer is not capable of training the model on the whole dataset, we suggest you to use a google colab with graphics card for that.
+Also consider that it's not necessary to train the model on the full image size, you can resize it to make it faster.
+Training with mobile net as head on whole dataset takes about 3 minutes per epoch if image downscaled to 128x128.
+
+## Evaluation
+
+In `frames` folder you can find 300 frames of X-wing flying in space. 
+Your goal is to track the X-wing in these frames with your NN model and output animation with lasers added.
+There is function in `utils.py` that will help you to evaluate your model. The only thing you need to do is to pass frames and coresponding points to it.
+Function `produce_animation` will return animation with lasers added.
--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import cv2
+import numpy as np
+
+from celluloid import Camera
+from matplotlib import pyplot as plt
+
 def project_point(image, point, model_view_matrix, projection_matrix, transformation_mat=None) -> tuple:
     """
     This function is project 3d Points in world space to image plane
@@ -23,3 +29,86 @@ def project_point(image, point, model_view_matrix, projection_matrix, transforma
 def project_points(image, points, model_view_matrix, projection_matrix, transformation_mat=None) -> list:
     return [project_point(image, point, model_view_matrix, projection_matrix, transformation_mat)
             for point in points]
+
+
+lasers = np.float32((
+    ((-0.145, -0.1, -0.01), (-0.145, -0.3, -0.01)),
+    ((-0.145, -0.1,  0.01), (-0.145, -0.3,  0.01)),
+    (( 0.145, -0.1, -0.01), ( 0.145, -0.3, -0.01)),
+    (( 0.145, -0.1,  0.01), ( 0.145, -0.3,  0.01))
+))
+
+bounding_box_points_in_3d = np.float32(
+    [(-0.156, -0.182, -0.032),
+     ( 0.156, -0.182, -0.032),
+     (-0.156,  0.163, -0.032),
+     ( 0.156,  0.163, -0.032),
+     (-0.156, -0.182,  0.034),
+     ( 0.156, -0.182,  0.034),
+     (-0.156,  0.163,  0.034),
+     ( 0.156,  0.163,  0.034)])
+
+K = np.array([
+    [497.7778,   0.0000, 256.0000],
+    [0.0000, 746.6667, 256.0000],
+    [0.0000,   0.0000,   1.0000]
+    ], dtype=np.float32)
+
+def laser_generator(origin, direction, rvect, tvec, max_len = 0.2, step_mul=0.2):
+    o, d = origin.copy(), direction.copy()
+    step = (direction - origin) * step_mul
+    d = o.copy()
+    yield np.array([o, d]), rvect, tvec
+    while True:
+        if np.linalg.norm(d - o) > max_len:
+            o += step
+        d += step
+        yield np.array([o, d]), rvect, tvec
+
+def rays_shooting_probability(rays_amount, period=25, variance=5):
+    i = 0
+    while True:
+        if i % period == 0:
+            next_shoot = np.random.randint(i, i+variance, rays_amount)
+        shoot = i == next_shoot
+        yield shoot
+        i += 1
+
+def produce_animation(frames, predicted_points_projections):
+    """
+    This function is produce animation of shooting xwing lasers based on predicted points projections
+    :param frames: Images of flying xwing
+    :param predicted_points_projections: predicted bounding box points projections
+    :return: Animation that could be saved or displayed with IPython.display.HTML in notebook
+    """
+    fig = plt.figure(figsize=(10, 10))
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+    plt.axis('off')
+    camera = Camera(fig)
+
+    laser_generators = []
+    rays_prob_gen = rays_shooting_probability(lasers.shape[0])
+
+    for fnum, (frame, pred_points) in enumerate(zip(frames, predicted_points_projections)):
+        img = frame.copy()
+
+        success, rotation_vector, translation_vector = cv2.solvePnP(bounding_box_points_in_3d,
+                                                                    pred_points, K, distCoeffs=None,
+                                                                    flags=cv2.SOLVEPNP_SQPNP)
+
+        for l, prob in zip(lasers, next(rays_prob_gen)):
+            if prob:
+                laser_generators.append(laser_generator(l[0], l[1], rotation_vector, translation_vector))
+
+        for lg in laser_generators:
+            lpts, rvec, tvec = next(lg)
+            points = cv2.projectPoints(lpts, rvec, tvec, K, distCoeffs=None)[0]
+            if points.max() > 1000:
+                continue
+            p1 = points[0][0].astype(int)
+            p2 = points[1][0].astype(int)
+            img = cv2.line(img, tuple(p1), tuple(p2), (0, 255, 0), 1, cv2.LINE_AA)
+        plt.imshow(img)
+        camera.snap()
+
+        return camera.animate(interval=33)
